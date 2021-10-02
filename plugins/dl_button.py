07b1e801968b6abc6f6a5e3e315a9360f4ff1072
ISLOMBEKS <91218515+ISLOMBEKS@users.noreply.github.com>
2021-10-02 23:02:17 +0500

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -256,9 +256,9 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
         await bot.edit_message_text(
             chat_id,
             message_id,
-            text="""Initiating Download
-URL: {}
-File Size: {}""".format(url, humanbytes(total_length))
+            text="""♻️Yuklab olishni boshlash
+📎URL manzili: {}
+💾Fayl hajmi: {}""".format(url, humanbytes(total_length))
         )
         with open(file_name, "wb") as f_handle:
             while True:
